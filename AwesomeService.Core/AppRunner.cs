1037931f1b056ea7be9c7e7aa535e2d2519c601d
Shems E. Boukhatem <contact@shemseddine.com>
2018-08-08 20:29:10 +0100

    Add project files.

---
+++ AwesomeService.Core/AppRunner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace AwesomeService.Core
+{
+    public class AppRunner
+    {
+        private Timer _timer;
+
+        public AppRunner()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("Service running");
+        }
+
+        public bool IsRunning => _timer.Enabled;
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ AwesomeService.WindowsService/Program.cs
@@ -0,0 +1,37 @@
+﻿using AwesomeService.Core;
+using System;
+using System.Configuration;
+using Topshelf;
+
+namespace AwesomeService.WindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runner = new AppRunner();
+
+            var serviceName = ConfigurationManager.AppSettings["ServiceName"];
+            var serviceDisplayName = ConfigurationManager.AppSettings["ServiceDisplayName"];
+            var serviceDescription = ConfigurationManager.AppSettings["ServiceDescription"];
+
+            var serviceRunner = HostFactory.Run(config =>
+            {
+                config.Service<AppRunner>(service =>
+                {
+                    service.ConstructUsing(name => runner);
+                    service.WhenStarted(serviceConfig => serviceConfig.Start());
+                    service.WhenStopped(serviceConfig => serviceConfig.Stop());
+                });
+                config.RunAsLocalSystem();
+
+                config.SetServiceName(serviceName);
+                config.SetDisplayName(serviceDisplayName);
+                config.SetDescription(serviceDescription);
+            });
+
+            var exitCode = (int)Convert.ChangeType(serviceRunner, serviceRunner.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
